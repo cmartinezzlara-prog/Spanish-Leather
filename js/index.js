6d75296f9f0f622a7eb986256053c7c73c888493
Cristina Martínez Lara <cmartinezzlara@alumnos.cei.es>
2026-01-18 01:26:55 +0100

    17/01-2
    Adaptando el proyecto a los criterios de evaluación.

--- js/index.js
+++ js/index.js
@@ -2,29 +2,32 @@
 
 
 /*
-1. Estoy en el menú.
+1. Estoy en MENU.
 
-2. Pulso Escuchar.
+2. Pulso "Escuchar".
 
 3. Veo cómo Babieca entra desde la derecha.
 
-4. Mientras Babieca entra, sigo viendo el menú.
+4. Mientras Babieca entra, sigo viendo MENU.
 
 5. Babieca cruza toda la pantalla.
 
 6. Solo cuando la última “A” empieza a salir por la izquierda:
-   - dejo de ver el menú
-   - empiezo a ver aparecer el Player por detrás
+   - dejo de ver el MENU
+   - empiezo a ver aparecer el PLAYER
 
 7. Babieca sale completamente por la derecha.
 
-8. Por primera vez veo el Player limpio.
+8. Por primera vez veo PLAYER.
 */
 
 
-
+// VARIABLES 
+// Btn "Escuchar"
 const listenBtn = document.querySelector('.MenuListen-btn')
+// Transicion Babieca
 const transition = document.querySelector('.Transition')
+// Player y menú
 const player = document.querySelector('.Player')
 const menu = document.querySelector('.Menu')
 
@@ -33,22 +36,26 @@ console.log(transition)
 console.log(player)
 console.log(menu)
 
+
+// EVENTO
+// Añadir clases. Cuando hago click en escuchar se les aplica isListening -> transition entra / menu desaparece / player aparece
 listenBtn.addEventListener('click', () => {
     transition.classList.add('isListening')
     menu.classList.add('isListening')
     player.classList.add('isListening')
 })
 
-//Splash  ->  Cover
+// VARIABLES
+// Splash  ->  Cover
 const splash = document.querySelector('.Splash')
 const cover = document.querySelector('.Cover')
 const flash = document.querySelector('.Splash-flash')
-
 
 console.log(splash)
 console.log(cover)
 console.log(flash)
 
+// Mientras splash isHidden -> cover isVisible
 const showCover = () => {
     console.log('Show cover')
 
@@ -56,26 +63,23 @@ const showCover = () => {
     cover.classList.add("isVisible")
 }
 
+// EVENTO 
+// Fin de la animación del destello -> cover
 flash.addEventListener('animationend', () => {
     console.log('Animación acabada')
     showCover()
 
+    // Splash display none
     splash.addEventListener('transitionend', () => {
         splash.style.display = 'none'
     })
 })
-//animacion 4s acabada = "animationend"
 
-
-//cuando cover lleva 3s = translateY 2s (arriba completo) + Cover-stars/layout opcity 0 1.2s 
-// const showMenu = () =>{
-//     console.log('Show menu')
-
-//     cover.style.opacity
-// }
-
+// ESTADO
 let coverActivo = true
 
+// EVENTO
+// Cuando cover isHidden -> menu isVisible
 window.addEventListener('wheel', () => {
     if (!coverActivo) return
     cover.classList.add("isHidden")
@@ -86,7 +90,7 @@ window.addEventListener('wheel', () => {
 
 })
 
-
+// VARIABLES
 // GALERIA 
 const videos = document.querySelectorAll('.PlayerScreen-video')
 const dots = document.querySelectorAll('.PlayerSlider-dot')
@@ -94,41 +98,47 @@ const prevBtn = document.querySelector('.PlayerArrow--prev')
 const nextBtn = document.querySelector('.PlayerArrow--next')
 const transitionText = document.querySelector('.TransitionText')
 
+// ESTADO
 let index = 0
-//declaramos el video que esta activo ahora (0=primer video)
+// Declaramos el video que esta activo ahora (0=primer video)
 
 videos.forEach(v => v.muted = true)
 
+// FUNCIÓN
 function showVideo(i) {
 
+    // Orden de videos
     if (i < 0) i = videos.length - 1
     if (i >= videos.length) i = 0
 
-    //si i es menor de 0 esta por detras del video activo o primer video
-    // si i es mayor o = a 0 esta por denlante (si es 12 es el ultimo)
+    // Si i es menor de 0 esta por detras del video activo o primer video
+    // Si i es mayor o = a 0 esta por denlante (si es 12 es el ultimo)
 
+    // Reset
     videos.forEach(v => {
         v.classList.remove('isActive')
         v.muted = true
     })
 
-    //antes de activar el correcto tengo que ponerlo a 0 = reiniciarlo
+    // Antes de activar el correcto tengo que ponerlo a 0 = reiniciarlo
     videos[i].currentTime = 0
     videos[i].play()
-    //activar el correcto
+    // Activar el correcto
     videos[i].classList.add('isActive')
-    //desmutear el activo
+    // Desmutear el activo
     videos[i].muted = false
 
-    //quitar isActive de los puntos
+    // Quitar isActive de los puntos
     dots.forEach(dots => dots.classList.remove('isActive'))
-    //activo el correcto
+    // Activo el correcto
     dots[i].classList.add('isActive')
 
-    //sirve para guardar el indice actual
+    // Sirve para guardar el indice actual
     index = i
 }
 
+// EVENTOS
+// Flechas reproductor
 nextBtn.addEventListener('click', () => {
     showVideo(index + 1)
 })
@@ -137,7 +147,7 @@ prevBtn.addEventListener('click', () => {
     showVideo(index - 1)
 })
 
-
+// Cuando la transición termina -> el video 1 = play
 transition.addEventListener('animationend', () => {
     console.log("animation end en .Transition")
 
